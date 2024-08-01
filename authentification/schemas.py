95a5a9776e910a6eb4859fd70710978f54195f3d
Aboubacar Fabou Camara <kmahlite@gmail.com>
2024-08-01 17:57:17 +0200

    modif schemas.py

--- authentification/schemas.py
+++ authentification/schemas.py
@@ -12,107 +12,47 @@ class UtilisateurCreate(BaseModel):
     role: str
     mot_de_passe: str
 
-class Utilisateur(BaseModel):
-    id: int
-    quartier_id: Optional[int]
-    nom_prenom: str
-    email: str
-    tel: str
-    genre: bool
-    copie_PI: str
-    role: str
-
-    class Config:
-        orm_mode: True
-
 
 
 class PmeCreate(BaseModel):
-    nom_pme: str
-    description: str
-    zone_intervention: str
-    logo_pme: str
-
-class Pme(BaseModel):
     id: int
     nom_pme: str
     description: str
     zone_intervention: str
     logo_pme: str
     utilisateur_id: int
-
-    class Config:
-        orm_mode: True
 
 
 
 class ClientCreate(BaseModel):
-    type_client: bool
-    num_rccm: str
-    nom_entreprise: str
-    photo_profil: str
-
-class Client(BaseModel):
     id: int
     type_client: bool
     num_rccm: str
     nom_entreprise: str
     photo_profil: str
     utilisateur_id: int
-
-    class Config:
-        orm_mode: True
-
+     
 
 
 class VilleCreate(BaseModel):
-    nom: str
-
-class Ville(BaseModel):
     id: int
     nom: str
-
-    class Config:
-        orm_mode: True
 
 
 
 class CommuneCreate(BaseModel):
-    nom: str
-    ville_id: int
-
-class Commune(BaseModel):
     id: int
     nom: str
     ville_id: int
-
-    class Config:
-        orm_mode: True
-
 
 
 class QuartierCreate(BaseModel):
-    nom: str
-    commune_id: int
-
-class Quartier(BaseModel):
     id: int
     nom: str
     commune_id: int
 
-    class Config:
-        orm_mode: True
-
-
 
 class AbonnementCreate(BaseModel):
-    num_abonnement: str
-    montant_abonnement: int
-    debut_abonnement: date
-    fin_abonnement: date
-    status_abonnement: bool
-
-class Abonnement(BaseModel):
     id: int
     num_abonnement: str
     montant_abonnement: int
@@ -122,18 +62,9 @@ class Abonnement(BaseModel):
     pme_id: int
     client_id: int
 
-    class Config:
-        orm_mode: True
-
 
 
 class CalendrierCreate(BaseModel):
-    jour_passage: date
-    heure_passage: time
-    creation: date
-    mise_a_jour: date
-
-class Calendrier(BaseModel):
     id: int
     jour_passage: date
     heure_passage: time
@@ -141,20 +72,10 @@ class Calendrier(BaseModel):
     mise_a_jour: date
     pme_id: int
 
-    class Config:
-        orm_mode: True
-
-
 
 class ConfPassageCreate(BaseModel):
-    date_confirmation: date
-    confirmation: str
-
-class ConfPassage(BaseModel):
     id: int
     date_confirmation: date
     confirmation: str
     calendrier_id: int
 
-    class Config:
-        orm_mode: True
