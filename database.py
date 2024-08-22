b8fef9845056cc44929299014e545b303723333e
Aboubacar Fabou Camara <kmahlite@gmail.com>
2024-08-22 21:25:24 +0200

    mise a jour base de donnees

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/pme_api'
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost/pme_api")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
